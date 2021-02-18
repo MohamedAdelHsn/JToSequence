aadf1761b89df915862db0a9de17d94e98b7c7a8
Mohamed Adel Hassan <58120325+MohamedAdelHsn@users.noreply.github.com>
2021-02-18 11:52:23 +0200

    Update SmallFilesToSequenceFile.java

--- src/com/java/hadoopUtils/SmallFilesToSequenceFile.java
+++ src/com/java/hadoopUtils/SmallFilesToSequenceFile.java
@@ -49,7 +49,7 @@ public static void main(String[] args) throws Exception {
 	
 		if (args.length != 2) {
 
-			System.err.println("Usage:	MaxTemperature	<input	path>	<output	path>");
+			System.err.println("Usage: <input path>	<output	path>");
 			System.exit(-1);
 
 		}
